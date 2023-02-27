b489dcf5fb573dd94df3b5e71c204ae526347689
Weiberle17 <maxweiberle63@gmail.com>
2023-02-27 11:06:22 +0100

    works for now

--- semesterFour/Sander/src/main/java/com/Sander/Aufgaben/Auto.java
+++ semesterFour/Sander/src/main/java/com/Sander/Aufgaben/Auto.java
@@ -3,26 +3,30 @@ package com.Sander.Aufgaben;
 public class Auto extends Thread {
   private int number;
   private boolean parked = false;
+  private Parkhaus myParkhaus;
 
-  public Auto(int number) {
+  public Auto(int number, Parkhaus name) {
     this.number = number;
+    this.myParkhaus = name;
     start();
   }
 
   public void run() {
     for (int i = 0; i < 10; i++) {
       einparken();
-      System.out.println("Auto Nummer: " + number + " ist eingefahren.");
+      System.out.println("Es sind " + myParkhaus.getPlaetze() + " Plätze frei.");
       ausparken();
-      System.out.println("Auto Nummer: " + number + " ist ausgefahren.");
+      System.out.println("Es sind " + myParkhaus.getPlaetze() + " Plätze frei.");
     }
   }
 
   public void einparken() {
+    myParkhaus.einfahren();
     this.parked = true;
   }
 
   public void ausparken() {
+    myParkhaus.ausfahren();
     this.parked = false;
   }
 
--- semesterFour/Sander/src/main/java/com/Sander/Aufgaben/Parkhaus.java
+++ semesterFour/Sander/src/main/java/com/Sander/Aufgaben/Parkhaus.java
@@ -4,14 +4,26 @@ public class Parkhaus {
   private int plaetze = 10;
 
   public Parkhaus() {
-    Auto[] autos = new Auto[10];
+  }
 
-    for (int i = 0; i < autos.length; i++) {
-      autos[i] = new Auto(i);
-    }
+  public void einfahren() {
+    plaetze--;
+  }
+
+  public void ausfahren() {
+    plaetze++;
+  }
+
+  public int getPlaetze() {
+    return plaetze;
   }
 
   public static void main(String[] args) {
-    new Parkhaus();
+    Parkhaus p1 = new Parkhaus();
+    Auto[] autos = new Auto[10];
+
+    for (int i = 0; i < autos.length; i++) {
+      autos[i] = new Auto(i, p1);
+    }
   }
 }
