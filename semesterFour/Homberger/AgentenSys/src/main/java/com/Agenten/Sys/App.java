15e41bb7ffb68dc6bb540f84aa1d4c2e38958b90
Weiberle17 <maxweiberle63@gmail.com>
2023-03-07 09:31:08 +0100

    initial commit

---
+++ semesterFour/Homberger/AgentenSys/src/main/java/com/Agenten/Sys/App.java
@@ -0,0 +1,13 @@
+package com.Agenten.Sys;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
